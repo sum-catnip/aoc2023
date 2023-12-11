fc0586bba161ffb24d05a3ce187f2e2c769edfcb
sum-catnip <catnip@catnip.fyi>
2023-12-11 13:06:44 +0100

    day9

---
+++ c#/day9/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int Extrapolate(int[] items) => items switch
+    {
+        _ when items.All(x => x == 0) => 0,
+        _ => items.Last() + Extrapolate(items
+        .Zip(items.Skip(1))
+        .Select((pair) => pair.Second - pair.First)
+        .ToArray())
+    };
+
+// part1
+Console.WriteLine(
+    File.ReadAllLines("input.txt")
+        .Select(l => Extrapolate(l.Split(' ').Select(int.Parse).ToArray()))
+        .Sum());
+
+// part2
+Console.WriteLine(
+    File.ReadAllLines("input.txt")
+        .Select(l => Extrapolate(l.Split(' ').Select(int.Parse).Reverse().ToArray()))
+        .Sum());
